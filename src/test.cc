a6ce8c0675fb5d25a6cf6810e22dfa9830010418
Gejza'X <gejza@gejza.net>
2010-12-05 19:45:58 +0000

    init webcb

---
+++ src/test.cc
@@ -0,0 +1,25 @@
+/*
+   File name:  test.cc
+   Date:       2010/05/07 20:47
+   Subversion: $Id: $
+   Author:     Milan Dunghubel <milan@mfis.cz>
+
+   Copyright (C) 2010 Milan Dunghubel <milan@mfis.cz>
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+/*#include <xc/log.h>
+#include <xc/error.h>
+*/
+//int xc_test_main(int argc, const char* argv[])
+int main(int argc, const char* argv[])
+{
+    return 0;
+}
+
+
+
+
+
