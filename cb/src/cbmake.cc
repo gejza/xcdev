e909a383dc17fe4757244e9d956df3a1d6c5932e
Gejza'X <gejza@gejza.net>
2010-12-05 20:00:44 +0000

    cb

---
+++ cb/src/cbmake.cc
@@ -0,0 +1,16 @@
+/*
+   File name:  cbmake.cc
+   Date:       2010/12/05 21:00
+   Subversion: $Id: $
+   Author:     Milan Dunghubel <milan@mfis.cz>
+
+   Copyright (C) 2010 Milan Dunghubel <milan@mfis.cz>
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+
+
+
+
