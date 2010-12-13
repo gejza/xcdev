98052bde4b30b29cc1d36db9e2810d5a97fe3e24
Gejza'X <gejza@gejza.net>
2010-12-13 19:56:18 +0000

    presun cb na registry

---
+++ include/xc/registry.h
@@ -0,0 +1,30 @@
+/*
+   File name:  registry.h
+   Date:       2010/12/13 20:30
+   Subversion: $Id: $
+   Author:     Milan Dunghubel <milan@mfis.cz>
+
+   Copyright (C) 2010 Milan Dunghubel <milan@mfis.cz>
+*/
+
+#ifndef _XC_REGISTRY_H_
+#define _XC_REGISTRY_H_
+#pragma once
+
+#include "table.h"
+
+namespace xc {
+
+class CodeRegistry_t
+{
+public:
+    CodeRegistry_t() {}
+
+    // string table
+};
+
+} // namespace xc
+
+#endif // _XC_REGISTRY_H_
+/* end of registry.h */
+
