1ec96cbcb6a9d360c139e867582a5a1682bdbca8
Gejza'X <gejza@gejza.net>
2010-12-17 19:33:54 +0000

    Vytvoreni env

---
+++ include/xc/registry/env.h
@@ -0,0 +1,37 @@
+/*
+   File name:  env.h
+   Date:       2010/12/15 21:44
+   Subversion: $Id: $
+   Author:     Milan Dunghubel <milan@mfis.cz>
+
+   Copyright (C) 2010 Milan Dunghubel <milan@mfis.cz>
+*/
+
+#ifndef _XC_REGISTRY_ENV_H_
+#define _XC_REGISTRY_ENV_H_
+#pragma once
+
+#include <libconfig.h++>
+
+namespace xc {
+namespace registry {
+
+class Lookup_t;
+
+class Env_t
+{
+public:
+    Env_t();
+    virtual ~Env_t() {}
+
+    const Lookup_t& get() const;
+protected:
+    libconfig::Config _config;
+};
+
+} // namespace registry
+} // namespace xc
+
+#endif // _XC_REGISTRY_ENV_H_
+/* end of env.h */
+
